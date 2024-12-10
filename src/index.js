fc8d69f4946500abe2076629e864f3fbd104699f
dmaass01 <52751543+dmaass01@users.noreply.github.com>
2024-12-10 18:47:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,17 +8,15 @@ import {
 } from 'react-router-dom'
 
 import './style.css'
-import NotFound1, { NotFound } from './views/not-found'
+import NotFound1 from './views/not-found'
 import Home from './views/home'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={NotFound1} path="*" />
+        <Route path="*" component={NotFound1} />
         <Route component={Home} exact path="/" />
-        <Route component={NotFound} path="**" />
-        <Redirect to="**" />
       </Switch>
     </Router>
   )
